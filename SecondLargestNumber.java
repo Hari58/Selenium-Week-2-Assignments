d215383e92e150ca105cb0f551b5ecc36cc4853f
Hari58 <71202765+Hari58@users.noreply.github.com>
2020-09-13 19:14:50 +0530

    Add files via upload

---
+++ SecondLargestNumber.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+public class SecondLargestNumber {
+
+	public static void main(String[] args) {
+		
+		int[] data = {3,2,11,4,6,7};
+		//System.out.println(data.length);
+		int arraySize = data.length;
+		Arrays.sort(data);
+		System.out.println(data[arraySize-2]);
+
+	}
+
+}
